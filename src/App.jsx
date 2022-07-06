6da4f544614a0870c8254a567e3e4c5bb5259f46
marblekang <kangbm930@gmail.com>
2022-07-06 19:14:22 +0900

    chore: 사용하지 않는 코드 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import { useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+
 import Slider from "./components/Slider/Slider";
 function App() {
   return <Slider></Slider>;
