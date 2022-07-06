b85010f0370892ce284449ccf1081538ef135cee
marblekang <kangbm930@gmail.com>
2022-07-06 19:11:50 +0900

    style: 버튼 스타일 추가
    - prevButton , nextButton 스타일 추가

--- src/components/Slider/style.js
+++ src/components/Slider/style.js
@@ -1,7 +1,20 @@
 import styled from "styled-components";
-
+import { MdKeyboardArrowRight, MdKeyboardArrowLeft } from "react-icons/md";
 export const Wrap = styled.div`
   overflow: hidden;
+  .prevButton {
+    position: absolute;
+    top: 95px;
+    left: 0px;
+    color: wheat;
+  }
+
+  .nextButton {
+    position: absolute;
+    top: 95px;
+    right: 0px;
+    color: wheat;
+  }
 `;
 
 export const SlideContainer = styled.div`
@@ -10,12 +23,13 @@ export const SlideContainer = styled.div`
   transition: ${({ effect }) => effect};
 `;
 
-//transition: ${({ effect }) => effect};
 export const SlideBox = styled.div`
   width: 100vw;
+  height: 200px;
   float: left;
 `;
 
 export const SlideImg = styled.img`
   width: 100%;
+  height: 100%;
 `;
